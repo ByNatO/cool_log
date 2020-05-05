9d7cff7e0f83f52c7b5e92cbd7e03fb62fa3ab11
ByNatO <mine19n@gmail.com>
2020-05-05 20:45:16 +0000

    some change in base.html, edit_entry.html, new_entry.html and manage.py

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learning_log.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learning_log_project.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
